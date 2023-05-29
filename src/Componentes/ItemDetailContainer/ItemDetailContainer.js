f48bb9dc562abd2e906ea3a0db14055173482bca
Martin Ricart <martinnricart@gmail.com>
2023-05-29 17:10:31 -0300

    Correccion Firebase

--- src/Componentes/ItemDetailContainer/ItemDetailContainer.js
+++ src/Componentes/ItemDetailContainer/ItemDetailContainer.js
@@ -3,6 +3,50 @@ import { useEffect, useState } from 'react'
 import { getProductosById} from '../../asyncMock'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom'
+import { getDoc, doc } from 'firebase/firestore'
+import {db} from '../../services/firebase/firebaseConfig'
+
+
+
+const ItemDetailContainer = () =>{
+    const[ producto, setProducto] = useState(null)
+    const[ loading, setLoading] = useState(true)
+
+    const { itemId } = useParams()
+
+    useEffect(()=>{
+        setLoading(true)
+
+        const docRef = doc(db, 'productos', itemId)
+
+        getDoc(docRef)
+            .then(response => {
+                const data = response.data()
+                const productAdapted = {id: response.id, ...data}
+                setProducto(productAdapted)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+            .finally(()=>{
+                setLoading(false)
+            })
+    },[itemId])
+
+    return(
+        <div className='ItemDetailContainer' >
+            <ItemDetail {...producto} />
+        </div>
+    )
+}
+
+
+
+export default ItemDetailContainer
+
+
+
+/* DETALLE DE CADA PRODUCTO DE productos.json
 
 
 const ItemDetailContainer = () => {
@@ -26,5 +70,4 @@ const ItemDetailContainer = () => {
         </div>
     )
 }
-
-export default ItemDetailContainer
+*/
--- src/Componentes/itemListContainer/ItemListContainer.jsx
+++ src/Componentes/itemListContainer/ItemListContainer.jsx
@@ -4,6 +4,51 @@ import ItemList from "../ItemList/ItemList"
 
 import { useParams } from "react-router-dom"
 
+import {getDocs, collection, query, where} from 'firebase/firestore'
+import {db} from '../../services/firebase/firebaseConfig'
+
+const ItemListContainer = ({ bienvenida }) =>{
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const { categoryId } = useParams()
+
+    useEffect(()=>{
+            setLoading(true)
+
+            const collectionRef = categoryId ? query(collection(db,'productos'), where('category','==', categoryId))
+            : collection(db, 'productos')
+            
+            getDocs(collectionRef)
+                .then(response=> {
+                    const productsAdapted = response.docs.map(doc =>{
+                        const data = doc.data()
+                        return {id: doc.id, ...data}
+                    })
+                    setProductos(productsAdapted)
+                })
+                .catch(error =>{
+                    console.log(error)
+                })
+                .finally(()=>{
+                    setLoading(false)
+                })
+        }, [categoryId])
+        return(
+            <div>
+                {bienvenida}
+                 {productos.map(p=> <ItemList key={productos.id}productos={productos}/>)}
+             </div>
+        )
+}
+
+
+export default ItemListContainer
+
+
+
+/* TRAER PRODUCTOS DE productos.json
+
 const ItemListContainer = ({ bienvenida }) => {
     const [productos, setProductos] = useState([])
     
@@ -28,5 +73,4 @@ const ItemListContainer = ({ bienvenida }) => {
              </div>
         )
 }
-
-export default ItemListContainer
+*/
